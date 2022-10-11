02508d95a0e03fb29786b6fc78cf88adc8b80f47
laureltheodore <110010515+laureltheodore@users.noreply.github.com>
2022-10-11 11:47:30 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ def partition(array, low, high):
     i = low - 1
     for j in range(low, high):
         if array[j] <= pivot:
-            i = i + 1
+            i = i + 1 
             (array[i], array[j]) = (array[j], array[i])
     (array[i + 1], array[high]) = (array[high], array[i + 1])
     return i + 1
