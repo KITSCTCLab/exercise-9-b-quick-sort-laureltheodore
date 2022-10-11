33dbef958b031bcb3bd870cedc937b347b0296ef
laureltheodore <110010515+laureltheodore@users.noreply.github.com>
2022-10-11 11:49:33 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -14,7 +14,7 @@ def quick_sort(array, low, high):
     if low < high:
         pi = partition(array, low, high)
         quick_sort(array, low, pi - 1)
-        quick_sort(array, pi + 1,hig)
+        quick_sort(array, pi + 1,ig)
     return array
  
 input_data = input()
